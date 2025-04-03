ba34ad950f4cc66e5055086073769a8023218e09
bmmuceo <bmmuceo@gmail.com>
2025-04-02 20:57:28 -0400

    WeSplit : Updated

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOFPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tippercentages = [10, 15, 20, 25, 0]
+    
+    
     var body: some View {
         Form {
-            Text("Hello World!")
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                Picker("Number of People", selection: $numberOFPeople) {
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
+                }
+            }
+    
+            Section {
+                Text(checkAmount, format: .currency(code:
+                    Locale.current.currency?.identifier ?? "USD"))
+            }
         }
     }
 }
