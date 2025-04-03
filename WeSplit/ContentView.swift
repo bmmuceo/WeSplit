61fac0bbd5e347cdc5897eece06ae4060e1b2563
bmmuceo <bmmuceo@gmail.com>
2025-04-02 21:26:58 -0400

    WeSplit : Updated

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,28 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOFPeople = 2
+    @State private var numberOFPeople = 0
     @State private var tipPercentage = 20
     
     let tippercentages = [10, 15, 20, 25, 0]
     
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-                Picker("Number of People", selection: $numberOFPeople) {
-                    ForEach(2..<100) {
-                        Text("\($0) people")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOFPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                    .pickerStyle(.navigationLink) //
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code:
+                        Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-    
-            Section {
-                Text(checkAmount, format: .currency(code:
-                    Locale.current.currency?.identifier ?? "USD"))
-            }
+            .navigationTitle("WeSplit") // place under whats inside the NavStack
         }
     }
 }
